9c4e46835dbe849475519db7ee448336ebefefe8
Norman Walsh <ndw@nwalsh.com>
2016-09-22 11:06:54 +0100

    Support different builders and CNAMES

--- .travis/publish-docs.sh
+++ .travis/publish-docs.sh
@@ -13,16 +13,25 @@ if [ "$TRAVIS_REPO_SLUG" == "$GIT_PUB_REPO" ]; then
     if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         echo -e "Publishing docs...\n"
 
-        ROOT=/home/travis/build/ndw/docbook.org
+        ROOT=/home/travis/build/$TRAVIS_REPO_SLUG
 
         cd gh-pages
         rsync -ar --delete --exclude .git --exclude .travis --exclude .travis.yml --exclude build.gradle --exclude build --exclude gradle.properties --exclude .gradle --exclude .gitignore $ROOT/ ./
+
+        if [ "$GITHUB_CNAME" != "" ]; then
+            echo $GITHUB_CNAME > CNAME
+        fi
 
         git add --all .
         git commit -m "Successful travis build $TRAVIS_BUILD_NUMBER"
         git push -fq origin gh-pages
 
-        echo -e "Published docbook.org to gh-pages.\n"
+        echo -e "Published docbook.org to gh-pages\n"
+        if [ "$GITHUB_CNAME" != "" ]; then
+            echo -e "... at $GITHUB_CNAME"
+        else
+            echo -e "... not published to a top-level domain"
+        fi
     else
         echo -e "Publication cannot be performed on pull requests.\n"
     fi
